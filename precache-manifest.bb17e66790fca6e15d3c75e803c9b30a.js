87f40986bed44e743de72b0e853836d0318b1228
Frank Quednau <frank.quednau@isolutions.ch>
2019-01-07 21:09:52 +0100

    Updates

---
+++ precache-manifest.bb17e66790fca6e15d3c75e803c9b30a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6f1cd80bcb00cf370af5",
+    "url": "/remorse/static/js/runtime~main.6f1cd80b.js"
+  },
+  {
+    "revision": "944da76dfc318572ffac",
+    "url": "/remorse/static/js/main.944da76d.chunk.js"
+  },
+  {
+    "revision": "54da880d5b66f93ddbb3",
+    "url": "/remorse/static/js/1.54da880d.chunk.js"
+  },
+  {
+    "revision": "944da76dfc318572ffac",
+    "url": "/remorse/static/css/main.3051ed8b.chunk.css"
+  },
+  {
+    "revision": "317f0c2b7205bb391ca8506cb3af4d92",
+    "url": "/remorse/index.html"
+  }
+];

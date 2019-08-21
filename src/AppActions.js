34acfdb193db2f85fdf12bdefb86fbfbc025488a
Frank Quednau <frank.quednau@isolutions.ch>
2019-08-21 16:41:09 +0200

    clean-slate

--- src/AppActions.js
+++
@@ -1,8 +0,0 @@
-import {storeObject, APP_STATE_KEY  } from "./components/Utils";
-
-export function saveProgress() {
-  return (dispatch, getState) => {
-    storeObject(APP_STATE_KEY, getState());
-    dispatch({type: "NO_OP"});
-  }
-}
--- src/AppReducer.js
+++
@@ -1,7 +0,0 @@
-import { combineReducers } from "redux";
-import train from "./Training/TrainingReducer";
-import typing from "./TypingScreen/TypingReducer";
-
-
-const appReducer = combineReducers({ typing, train });
-export default appReducer;
